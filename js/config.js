0c32a60a8ed0853204231688b337bf2a96879c4b
mathieulecl <161606733+mathieulecl@users.noreply.github.com>
2024-10-08 17:49:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_1MMNDhAvo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4b1slcgcsougamkievvgq180bu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ue-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
